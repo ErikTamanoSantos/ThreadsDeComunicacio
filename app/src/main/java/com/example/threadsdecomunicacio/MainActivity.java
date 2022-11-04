da46169d7c3bcc913f90fd638a18c85addc6475e
eriktamano <eriktamano@gmail.com>
2022-11-04 18:51:35 +0100

    ListView afegit

--- app/src/main/java/com/example/threadsdecomunicacio/MainActivity.java
+++ app/src/main/java/com/example/threadsdecomunicacio/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.threadsdecomunicacio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +20,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -24,11 +34,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        ListView list = findViewById(R.id.listView);
+        ArrayList<String> listString = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, R.layout.list_item, listString)
+        {
+            @Override
+            public View getView(int pos, View convertView, ViewGroup container)
+            {
+                // getView ens construeix el layout i hi "pinta" els valors de l'element en la posició pos
+                if( convertView==null ) {
+                    // inicialitzem l'element la View amb el seu layout
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+                }
+                // "Pintem" valors (també quan es refresca)
+                ((TextView) convertView.findViewById(R.id.textView)).setText(listString.get(pos));
+                return convertView;
+            }
+
+        };
+        list.setAdapter(adapter);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getDataFromUrl("https://api.myip.com");
+            public void onClick(View v) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        String res = getDataFromUrl("https://api.myip.com");
+                        Log.i("INFO:", res);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                listString.add(res);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
             }
         });
 
